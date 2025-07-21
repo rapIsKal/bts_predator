f7ef7fb243623b3e4609ea4978d94e3907a7a4c2
vgorelov <rap-is-kal-again@yandex.ru>
2025-07-21 18:23:41 +0300

    try to trigger ci/cd

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -11,7 +11,8 @@ from main import contains_korean_and_arbeit_macht_frei
     "ищулюдейнаработуза5k rub",
     "5000рубработа",
     "ищуработусзарплатой15тыср",
-    "10.000 руб."
+    "10.000 руб.",
+    "10000 руб"
 ])
 def test_spam_detected(text):
     assert contains_korean_and_arbeit_macht_frei(text) is True
